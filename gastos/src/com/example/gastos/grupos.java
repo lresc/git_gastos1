43ca367a1ad3fadad403e85350228043da2b469d
lara <lara@lara-N61Jq>
2013-05-21 19:26:23 -0300

    Falta en editar: eliminar miembros

--- gastos/src/com/example/gastos/grupos.java
+++ gastos/src/com/example/gastos/grupos.java
@@ -1,8 +1,6 @@
 package com.example.gastos;
 
-
 //FALTA: cuando se pulse atras, que vaya a pagina principal.xml
-
 
 import android.app.ListActivity;
 import android.content.ContentValues;
@@ -22,140 +20,166 @@ import android.widget.SimpleCursorAdapter;
 import static com.example.gastos.Constantes.*;
 
 public class grupos extends ListActivity {
-	//ESTA ACTIVITY MOSTRARA UNA LISTA DE LOS GRUPOS
-	private static String[] FROM = {_ID,NOMBRE_GRUPO,MIEMBROS,EMAILS,SINCRONIZACION};// {_ID, NOMBRE_GRUPO, MIEMBROS, SINCRONIZACION}
-	private static int[] TO={R.id.gnombre, R.id.gmiembros};//ampliar a sincronización...
-	String ORDER_BY= NOMBRE_GRUPO + " ASC";
+	// ESTA ACTIVITY MOSTRARA UNA LISTA DE LOS GRUPOS
+	private static String[] FROM = { _ID, NOMBRE_GRUPO, MIEMBROS, EMAILS,
+			SINCRONIZACION };
+	private static int[] TO = { R.id.gnombre, R.id.gmiembros };// ampliar a
+																// sincronización...
+	String ORDER_BY = NOMBRE_GRUPO + " ASC";
 	Cursor cursor;
-	
-	private baseDatos groups= new baseDatos(this);
+
+	private baseDatos groups = new baseDatos(this);
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.grupos);
-		
-		Bundle extras=getIntent().getExtras();
-		if(extras!=null){
+		Log.d("GRUPOS", "1");
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			Log.d("GRUPOS", "2");
 			String nombre = extras.getString("name");
-			String miembros=extras.getString("members");
-			String sinc=extras.getString("sync");
-			String mails=extras.getString("mails");
-			if(nombre !=null && miembros!=null && sinc!=null){
-				agregarGrupo(nombre,miembros,mails,sinc);
-				Log.d("GRUPOS","sinSalio de agregar grupoces sYnc "+sinc);
-			}
-			else{
-				//falta algun campo por poner..
+			Log.d("GRUPOS", "3");
+			String miembros = extras.getString("members");
+			Log.d("GRUPOS", "4"+miembros);
+			String sinc = extras.getString("sync");
+			String mails = extras.getString("mails");
+			boolean editar = extras.getBoolean("editar");
+			
+			if (nombre.length() > 0 && miembros!=null) {
+				Log.d("GRUPOS", "NOMBRE NO ES NULL Y MIEMBROS TAMPOCO");
+				if (editar) {
+					Log.d("GRUPOS", "EDITAR!");
+					editarGrupo(extras.getInt("id"),nombre,miembros,mails,sinc);
+				} else {
+					Log.d("GRUPOS", "AGREGAR!");
+						agregarGrupo(nombre, miembros, mails, sinc);
+					}
+			}else {
+				Log.d("GRUPOS", "NOMBRE o miembros ES NULL");
+					// falta algun campo por poner..
 			}
 		}
-		registerForContextMenu(getListView());		
-		Cursor c=obtenerGrupos();
-		mostrarGrupos(c);
-		}
-	
-	private void agregarGrupo(String nameg,String memb,String mail, String synq){
-		//TRAS nuevoGrupo al aceptar	
-
-		Log.d("GRUPOS","NEW BD");
-		SQLiteDatabase db=groups.getWritableDatabase();
-//		Log.d("GRUPOS","DESPUES DE writaDBLE");
-		ContentValues values=new ContentValues();
 		
+		registerForContextMenu(getListView());
+		Cursor c = obtenerGrupos();
+		mostrarGrupos(c);
+	}
+
+	private void editarGrupo(int id, String nameg, String memb, String mail,
+			String synq) {
+		Log.d("GRUPOS", "editarGrupo");
+		SQLiteDatabase db = groups.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
 		values.put(NOMBRE_GRUPO, nameg);
 		values.put(MIEMBROS, memb);
 		values.put(EMAILS, mail);
 		values.put(SINCRONIZACION, synq);
-		Log.d("GRUPOS",nameg+" "+memb+" "+mail+" "+synq);
-		db.insertOrThrow(TABLA_GRUPOS, null, values);
-		db.close();
+		Log.d("GRUPOS", nameg + " " + memb + " " + mail + " " + synq);
+
+		db.update(TABLA_GRUPOS, values, _ID + "=" + id, null);
 	}
-	
-	private Cursor obtenerGrupos(){
-		
-		SQLiteDatabase db= groups.getReadableDatabase();
-		cursor =db.query(TABLA_GRUPOS, FROM, null,null, null, null, ORDER_BY);
-		Log.d("GRUPOS","QUERY ");
-		startManagingCursor(cursor);	
+
+	private void agregarGrupo(String nameg, String memb, String mail,
+			String synq) {
+		// TRAS nuevoGrupo al aceptar
+
+		Log.d("GRUPOS", "NEW BD");
+		SQLiteDatabase db = groups.getWritableDatabase();
+		// Log.d("GRUPOS","DESPUES DE writaDBLE");
+		ContentValues values = new ContentValues();
+
+		values.put(NOMBRE_GRUPO, nameg);
+		values.put(MIEMBROS, memb);
+		values.put(EMAILS, mail);
+		values.put(SINCRONIZACION, synq);
+		Log.d("GRUPOS", nameg + " " + memb + " " + mail + " " + synq);
+		db.insertOrThrow(TABLA_GRUPOS, null, values);
+		// db.close();
+	}
+
+	private Cursor obtenerGrupos() {
+
+		SQLiteDatabase db = groups.getReadableDatabase();
+		cursor = db.query(TABLA_GRUPOS, FROM, null, null, null, null, ORDER_BY);
+		Log.d("GRUPOS", "QUERY ");
+		startManagingCursor(cursor);
 
 		return cursor;
-		
+
 	}
-	
-	private void mostrarGrupos(Cursor cursor){		
+
+	private void mostrarGrupos(Cursor cursor) {
 		cursor.moveToFirst();
-		SimpleCursorAdapter adapter=new SimpleCursorAdapter(this,R.layout.item, cursor,new String[]{NOMBRE_GRUPO,MIEMBROS},TO);//hay otro import
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.item, cursor, new String[] { NOMBRE_GRUPO, MIEMBROS },
+				TO);// hay otro import
 		setListAdapter(adapter);
-		        
+
 	}
-	
-	
-	
-	
+
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_grupos, menu);
 	}
-	
-	    
+
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
 
-	Log.d("GRUPOS","SELECTitem");	
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-		switch(item.getItemId()) {
-			case R.id.menu_verMiembros:///mejor sin esto... => quitar miembro.java y item_miembros.xml
-				String e=cursor.getString(2);
-				Log.d("GRUPOS","lo que se envia "+e);	
-				Intent i0=new Intent(this, miembros.class);
-				i0.putExtra("miembros",cursor.getString(2));
-				
-				startActivity(i0);				
-		
+		Log.d("GRUPOS", "SELECTitem");
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+		switch (item.getItemId()) {
+		case R.id.menu_verMiembros:// /mejor sin esto... => quitar miembro.java
+									// y item_miembros.xml
+			String e = cursor.getString(2);
+			Log.d("GRUPOS", "lo que se envia " + e);
+			Intent i0 = new Intent(this, miembros.class);
+			i0.putExtra("miembros", cursor.getString(2));
+
+			startActivity(i0);
+
 			return true;
-			case R.id.menu_verGastos:
-				Intent i = new Intent(this, gastos.class);
-				//poner de extra el id de grupo
-				startActivity(i);
-				return true; 
-				
-			case R.id.menu_editarGrupo:
-				Intent i1 = new Intent(this, nuevoGrupo.class);
-				cursor.moveToFirst();
-				cursor.move(info.position);
-				
-				i1.putExtra("nombre", cursor.getString(1));
-				i1.putExtra("miembros", cursor.getString(2));
-				i1.putExtra("mail", cursor.getString(3));
-				i1.putExtra("sync", cursor.getString(4));
-				cursor.moveToFirst();
-				cursor.move(info.position);
-				startActivity(i1);
-				return true;
-				
-			case R.id.menu_borrarGrupo:
+		case R.id.menu_verGastos:
+			Intent i = new Intent(this, gastos.class);
+			// poner de extra el id de grupo
+			startActivity(i);
+			return true;
 
-				cursor.moveToFirst();
-				cursor.move(info.position);
-				borrarGrupo(cursor.getInt(0));
-				cursor=obtenerGrupos();
-				mostrarGrupos(cursor);
-				return true;
-				
-			default:
-				return super.onContextItemSelected(item);
+		case R.id.menu_editarGrupo:
+			Intent i1 = new Intent(this, nuevoGrupo.class);
+			cursor.moveToFirst();
+			cursor.move(info.position);
+			i1.putExtra("id", cursor.getInt(0));
+			i1.putExtra("nombre", cursor.getString(1));
+			i1.putExtra("miembros", cursor.getString(2));
+			i1.putExtra("mail", cursor.getString(3));
+			i1.putExtra("sync", cursor.getString(4));
+			startActivity(i1);
+			return true;
+
+		case R.id.menu_borrarGrupo:
+
+			cursor.moveToFirst();
+			cursor.move(info.position);
+			borrarGrupo(cursor.getInt(0));
+			cursor = obtenerGrupos();
+			mostrarGrupos(cursor);
+			return true;
+
+		default:
+			return super.onContextItemSelected(item);
 		}
-				
-	}
-	
-	private int borrarGrupo(int id){
-		SQLiteDatabase db=groups.getWritableDatabase();
-		//Log.d("GRUPOS", id);
-		return db.delete(TABLA_GRUPOS, _ID +"='"+id+"'", null);
+
 	}
 
+	private int borrarGrupo(int id) {
+		SQLiteDatabase db = groups.getWritableDatabase();
+		// Log.d("GRUPOS", id);
+		return db.delete(TABLA_GRUPOS, _ID + "='" + id + "'", null);
+	}
 
 }
-
-
-
--- gastos/src/com/example/gastos/nuevoGrupo.java
+++ gastos/src/com/example/gastos/nuevoGrupo.java
@@ -17,68 +17,59 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
-
 public class nuevoGrupo extends Activity {
 	String miembros = null;
 	String mails = null;
 	String nombre = null;
 	String sinc = null;
-	List<Map<String, String>> data=new ArrayList<Map<String, String>>();
+	List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+	boolean editar;
+	int id;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.nuevogrupo);
-		Log.d("GRUPOS","onCreate");
-	
+		Log.d("GRUPOS", "onCreate");
+		editar=false;
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
 			nombre = extras.getString("nombre");
 			miembros = extras.getString("miembros");
 			mails = extras.getString("mail");
 			sinc = extras.getString("sync");
-			Log.d("GRUPOS","onCreate2");
+			id=extras.getInt("id");
 			escribir();
 		}
-		Log.d("GRUPOS","onCreate1");
+		Log.d("GRUPOS", "onCreate1");
 	}
 
 	private void escribir() {
-		Log.d("GRUPOS","escribir");
+		int i = 0;
 		EditText e1 = (EditText) this.findViewById(R.id.gname);
 		Button b1 = (Button) this.findViewById(R.id.sincronizarMiembro);
-		Log.d("GRUPOS","escribir");
+		editar=true;//ponemos a true
+		
 		// nombre del grupo
 		e1.setText(nombre);
+		
 		// parte miembros y mails
-		
-		Log.d("GRUPOS",miembros);
-	
-		//Log.d("GRUPOS",mails);
 		String[] m = miembros.split(",");
-		String[]ma=null;
-		if(mails!=null)
-			ma=mails.split(",");
-		
-		int i=0;
-		Log.d("GRUPOS","escribir");
-		
-		//ponemos a null las variables..
-		miembros=null;
-		mails=null;
-		
-		for (String t: m) {		
-			Log.d("GRUPOS","addmember "+ t);
-			addMembers(t,ma[i]);
-		//	addMembers(t,null);
-			Log.d("GRUPOS","addmember "+i);
+		String[] ma = mails.split(",");
+
+		// ponemos a null las variables..
+		miembros = null;
+		mails = null;
+
+		for (String t : m) {
+			addMembers(t, ma[i]);
 			i++;
 		}
 
 		// parte sincronizar
 		if (sinc.equals("true")) {
-			b1.setText(this.getString(R.string.sync));
-		} else {
 			b1.setText(this.getString(R.string.notsync));
+		} else {
+			b1.setText(this.getString(R.string.sync));
 		}
 
 	}
@@ -92,15 +83,11 @@ public class nuevoGrupo extends Activity {
 		final EditText e1 = (EditText) prueba.findViewById(R.id.name);
 		final EditText e2 = (EditText) prueba.findViewById(R.id.email);
 		final Button button = (Button) prueba.findViewById(R.id.acept2);
-		Log.d("GRUPOS","CLICKMIEMBRO1");
 		button.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Log.d("GRUPOS","CLICKMIEMBROADD");
-				// TODO Auto-generated method stub
 				addMembers(e1.getText().toString(), e2.getText().toString());
-				Log.d("GRUPOS","CLICKMIEMBROADDNO");
 				prueba.cancel();
 			}
 		});
@@ -109,45 +96,41 @@ public class nuevoGrupo extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				Log.d("GRUPOS","CLICKMIEMBRO_CANCEL");
+				Log.d("GRUPOS", "CLICKMIEMBRO_CANCEL");
 				prueba.cancel();
-				
+
 			}
 		});
 	}
 
 	public void addMembers(String m, String mail) {
-		Log.d("GRUPOS", "addMembers0");
-	
-		Log.d("GRUPOS", "addMembers1");
-		Map<String, String> datum = new HashMap<String, String>(2);
-		if (m != null ) {
-			if (miembros == null&&mails==null) {
+
+		if (m.length() > 0) {
+			if (mail.length() == 0)
+				mail = "-";
+
+			Map<String, String> datum = new HashMap<String, String>(2);
+			if (miembros == null) {
 				miembros = m + ",";
-				mails = mail + ","; 
-				
+				mails = mail + ",";
+
 			} else {
-				miembros = miembros + m + ","; 
+				miembros = miembros + m + ",";
 				mails = mails + mail + ",";
-			
+
 			}
-					 
-			Log.d("GRUPOS", "addMember2");
+
 			datum.put("miembro", m);
 			datum.put("mail", mail);
-			Log.d("GRUPOS", "addMember3");
 			data.add(datum);
-			Log.d("GRUPOS", "addMember4");
 			SimpleAdapter adapter = new SimpleAdapter(this, data,
-					android.R.layout.simple_list_item_2, new String[] { "miembro",
-							"mail" }, new int[] { android.R.id.text1,
-							android.R.id.text2 });
-			Log.d("GRUPOS", "addMember5");
+					android.R.layout.simple_list_item_2, new String[] {
+							"miembro", "mail" }, new int[] {
+							android.R.id.text1, android.R.id.text2 });
 			ListView l1 = (ListView) this.findViewById(R.id.members);
 			l1.setAdapter(adapter);
-			Log.d("GRUPOS", "addMember6");
 		}
-		
+
 	}
 
 	public void clickSincMiembro(View v) {
@@ -164,27 +147,22 @@ public class nuevoGrupo extends Activity {
 	public void clickNuevoGrupo(View v) {
 		
 		EditText e1 = (EditText) this.findViewById(R.id.gname);
-	//	Log.d("GRUPOS","CLICK1");
 		Button b1 = (Button) this.findViewById(R.id.sincronizarMiembro);
-		//Log.d("GRUPOS","CLICK1");
 		Intent i = new Intent(this, grupos.class);
-		Log.d("GRUPOS","CLICK1");
+		i.putExtra("editar", editar);
+		i.putExtra("id", id);//id del grupo a editar en la clase grupo, solo se agarrara para el caso en q editar sea true
 		i.putExtra("name", e1.getText().toString());
-		Log.d("GRUPOS","CLICK1 "+miembros);
 		i.putExtra("members", miembros);
-		Log.d("GRUPOS","CLICKmail "+mails);
 		i.putExtra("mails", mails);
-		Log.d("GRUPOS","CLICK1");
-		
+
 		if (b1.getText().toString() == this.getString(R.string.sync)) {
 			// no sincronización
-			Log.d("GRUPOS", "es false");
 			i.putExtra("sync", "false");
 		} else {
 			// sincronizacion
-			Log.d("GRUPOS", "es true");
 			i.putExtra("sync", "true");
 		}
+		Log.d("GRUPOS", "STARTACTIVITY");
 		startActivity(i);
 	}
 }
